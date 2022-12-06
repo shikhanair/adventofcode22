f2e7501052c9a069a20bb9189b2bd75572648d1b
Shikha <shikhavnair@gmail.com>
2022-12-06 08:52:35 -0500

    day 6

---
+++ day6.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    with open("inputs/day6in") as f:
+        data = f.read()
+    for i in range(len(data)):
+        if len(set(data[i:i+4])) == 4:
+            print(i + 4)
+            break;
+    for i in range(len(data)):
+        if len(set(data[i:i+14])) == 14:
+            print(i + 14)
+            break;
