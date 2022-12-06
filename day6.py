fcc42e242baec9c0513e0a71032169bd5570128f
Shikha <shikhavnair@gmail.com>
2022-12-06 09:23:33 -0500

    day 6

--- day6.py
+++ day6.py
@@ -1,11 +1,11 @@
+def find_marker(len_data, len_window):
+    for i in range(len_data):
+        if len(set(data[i:i+len_window])) == len_window:
+            return i + len_window
+
 if __name__ == '__main__':
     with open("inputs/day6in") as f:
         data = f.read()
-    for i in range(len(data)):
-        if len(set(data[i:i+4])) == 4:
-            print(i + 4)
-            break
-    for i in range(len(data)):
-        if len(set(data[i:i+14])) == 14:
-            print(i + 14)
-            break
+
+    print(find_marker(len(data), 4))
+    print(find_marker(len(data), 14))
